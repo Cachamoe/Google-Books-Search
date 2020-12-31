919e76eab93c703ecdf2d6dc5da257273c5e3dc0
Caleb Chaney <calebc347@gmail.com>
2020-12-30 19:47:16 -0600

    Updated Header component, App.js and package.json

--- client/src/pages/saved.js
+++ client/src/pages/saved.js
@@ -1,1 +1,3 @@
 // Renders all books saved to the Mongo database. User has an option to "View" the book, bringing them to the book on Google Books, or "Delete" a book, removing it from the Mongo database.
+import React, { useEffect } from "react";
+import "./style.css"
