76f51163eacf40e48d924f6999355285b071617a
Caleb Chaney <calebc347@gmail.com>
2020-12-31 10:24:51 -0600

    Created models and routes and updated server

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/controller");
+
+router.route("/books")
+    .get(controller.findAll)
+    .post(controller.save)
+
+router.route("/books/:id")
+    .get(controller.findById)
+    .put(controller.update)
+    .delete(controller.remove)
+
+module.exports = router;
