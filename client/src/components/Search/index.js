572f1820077b0ca8f3e3fd701c3d786ef68013e3
Caleb Chaney <calebc347@gmail.com>
2021-01-04 12:05:22 -0600

    Updated Card and Search components

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -4,10 +4,13 @@ import "./style.css";
 export function SearchBar(props) {
     return (
         <>
-        <div className="input-group input-group-lg col-md-11 searchBar">
-        <h3 className="margin">Book Search:</h3>
-            <input className="form-control" type="text" {...props} />
-        </div>
+            <div className="input-group input-group-lg col-md-11 searchBar">
+                <h3 className="margin">Book Search:</h3>
+                <input className="form-control" type="text" {...props} />
+            </div>
+            <button className="btn btn-secondary me-md-2 button" type="button">
+                Search
+            </button>
         </>
     );
 }
